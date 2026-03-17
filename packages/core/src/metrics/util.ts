7d1d3bfe0d89c472e33555dd2f27525a7308c134
pcfulife <pcfulife@gmail.com>
2026-03-17 13:50:18 +0900

    [#10] Fix: trimUrl always returns withStartingSlashUrl

--- packages/core/src/metrics/util.ts
+++ packages/core/src/metrics/util.ts
@@ -51,10 +51,6 @@ export function getStatusCodeGroup(statusCode: number) {
 }
 
 export const trimUrl = (url: string, maxDepth?: number) => {
-    if (typeof maxDepth === 'number') {
-        const withStartingSlashUrl = url.startsWith('/') ? url : '/' + url
-        return withStartingSlashUrl.split('/', maxDepth + 1).join('/')
-    } else {
-        return url
-    }
+    const withStartingSlashUrl = url.startsWith('/') ? url : '/' + url
+    return typeof maxDepth === 'number' ? withStartingSlashUrl.split('/', maxDepth + 1).join('/') : withStartingSlashUrl
 }
